d3fe9a15f53f9bbd433b6927a405b202c6bc3277
EvroDev <78891194+EvroDeveloper@users.noreply.github.com>
2024-07-23 18:58:33 -0400

    Update Tracker.cs

--- Scripts/SLZ.Marrow/SLZ/Marrow/Interaction/Tracker.cs
+++ Scripts/SLZ.Marrow/SLZ/Marrow/Interaction/Tracker.cs
@@ -83,8 +83,8 @@ namespace SLZ.Marrow.Interaction
 			BoxCollider boxCollider = _collider as BoxCollider;
 			if (boxCollider != null)
 			{
-				boxCollider.center = bounds.center;
-				boxCollider.size = bounds.size;
+				boxCollider.center = Vector3.Scale(bounds.center, new Vector3(1/transform.parent.localScale.x, 1 / transform.parent.localScale.y, 1 / transform.parent.localScale.z));
+				boxCollider.size = Vector3.Scale(bounds.size, new Vector3(1 / transform.parent.localScale.x, 1 / transform.parent.localScale.y, 1 / transform.parent.localScale.z));
 			}
 #endif
 		}
